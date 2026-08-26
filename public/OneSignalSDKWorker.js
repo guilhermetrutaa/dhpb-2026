09838dc384186a71bd74119868318e05d129a26c
guilhermetrutaa <guilhermetrutaa@gmail.com>
2026-08-26 18:59:31 -0300

    Fix: Ajeitando +1 erro

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -2,4 +2,6 @@
   OneSignal Service Worker
   Reference: https://documentation.onesignal.com/docs/service-worker-setup
 */
-self.importScripts("/OneSignalSDK.sw.js");
+// Mantido para instalações antigas registradas no escopo raiz.
+// O SDK local anterior estava corrompido; carregue sempre o arquivo oficial.
+self.importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
